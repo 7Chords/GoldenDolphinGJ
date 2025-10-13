c8c12856f5d810e64887bb6d89c9888ee3a337c3
四不和声 <1223736337@qq.com>
2025-10-13 19:43:30 +0800

    dev:配表工具

---
+++ Assets/Framework/Export/SCRefDataMgr.cs
@@ -0,0 +1,15 @@
+namespace GJFramework
+{
+    /// <summary>
+    /// 配表数据管理器 在这里写所有的配表数据
+    /// </summary>
+    public class SCRefDataMgr : Singleton<SCRefDataMgr>
+    {
+
+
+        public void Init()
+        {
+
+        }
+    }
+}
