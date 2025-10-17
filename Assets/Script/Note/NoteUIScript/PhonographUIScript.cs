5bfd66b5413527dc819d4afc8ab361abdbe0a20f
YuzeNeko <1992271758@qq.com>
2025-10-18 00:06:28 +0800

    dev: CollectPanel

---
+++ Assets/Script/Note/NoteUIScript/PhonographUIScript.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class PhonographUIScript : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        // todo:
+        // 打开留声机 暂停动画
+
+    }
+}
