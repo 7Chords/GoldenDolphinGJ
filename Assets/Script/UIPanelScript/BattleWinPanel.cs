7150751d165499b9dbdb6cbaca0779aaacbfe592
四不和声 <1223736337@qq.com>
2025-10-18 19:57:40 +0800

    dev:胜利界面

--- Assets/Script/UIPanelScript/BattleWinPanel.cs
+++ Assets/Script/UIPanelScript/BattleWinPanel.cs
@@ -1,14 +1,31 @@
+using DG.Tweening;
 using GJFramework;
 using System;
+using UnityEngine;
 
 public class BattleWinPanel : UIPanelBase
 {
+
+    public CanvasGroup canvasGroup;
+
+    private TweenContainer _tweenContainer;
+
+    public float fadeInDuration;
+    public float fadeOutDuration;
+
     protected override void OnShow()
     {
+        canvasGroup.alpha = 0;
+        _tweenContainer = new TweenContainer();
+        _tweenContainer.RegDoTween(canvasGroup.DOFade(1, fadeInDuration));
 
     }
     protected override void OnHide(Action onHideFinished)
     {
+        _tweenContainer.RegDoTween(canvasGroup.DOFade(0, fadeOutDuration).OnComplete(() =>
+        {
+            onHideFinished?.Invoke();
+        }));
 
     }
 }
