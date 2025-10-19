ad5c19a9c6210c3f997e803c8e7aaa98abe9710f
四不和声 <1223736337@qq.com>
2025-10-19 12:05:34 +0800

    dev：播放视频

--- Assets/Script/UIPanelScript/StartPanel.cs
+++ Assets/Script/UIPanelScript/StartPanel.cs
@@ -1,18 +1,38 @@
 using GJFramework;
 using System;
 using UnityEngine.UI;
+using UnityEngine.Video;
 
 public class StartPanel : UIPanelBase
 {
     public Button btnGo;
+
+    private VideoPlayer _videoPlayer;
     protected override void OnShow()
     {
         btnGo.onClick.AddListener(() =>
         {
+            btnGo.enabled = false;
             AudioMgr.Instance.PlaySfx("开始游戏");
-            SceneLoader.Instance.AddNextScenePanel(EPanelType.LevelSelectPanel);
-            TransitionMgr.Instance.StarTransition("LevelSelectScene", "FadeInAndOutTransition");
+            _videoPlayer = FindObjectOfType<VideoPlayer>();
+            _videoPlayer.time = 0;
+            _videoPlayer?.Play();
+            AudioMgr.Instance.PauseBgm();
+
+            _videoPlayer.loopPointReached += EndWithVideoPlay;
         });
+
+
+        //SceneLoader.Instance.AddNextScenePanel(EPanelType.LevelSelectPanel);
+        //TransitionMgr.Instance.StarTransition("LevelSelectScene", "FadeInAndOutTransition");
+    }
+
+    public void EndWithVideoPlay(VideoPlayer source)
+    {
+        AudioMgr.Instance.ResumeBgm();
+
+        SceneLoader.Instance.AddNextScenePanel(EPanelType.LevelSelectPanel);
+        TransitionMgr.Instance.StarTransition("LevelSelectScene", "FadeInAndOutTransition");
     }
     protected override void OnHide(Action onHideFinished)
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           {
