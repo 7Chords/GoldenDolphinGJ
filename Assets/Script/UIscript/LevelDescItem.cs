d1ccb8c9b7aed3c98b4c2f872f34f800a243d58c
四不和声 <1223736337@qq.com>
2025-10-21 23:09:12 +0800

    dev:关卡信息item

---
+++ Assets/Script/UIscript/LevelDescItem.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelDescItem : MonoBehaviour
+{
+
+
+    public void SetInfo(BattleLevelRefObj _levelRefObj)
+    {
+
+
+    }
+}
