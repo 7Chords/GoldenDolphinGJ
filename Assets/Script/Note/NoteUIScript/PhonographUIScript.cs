494bae628d0dfbfa1081de3dfb21b58beeca37b1
YuzeNeko <1992271758@qq.com>
2025-10-23 23:56:21 +0800

    dev: 收集音符界面初始为暂停

--- Assets/Script/Note/NoteUIScript/PhonographUIScript.cs
+++ Assets/Script/Note/NoteUIScript/PhonographUIScript.cs
@@ -5,9 +5,10 @@ using UnityEngine.UI;
 
 public class PhonographUIScript : MonoBehaviour, IPointerClickHandler
 {
-    bool currentPauseState = false;
+    bool currentPauseState = true;
     public void OnPointerClick(PointerEventData eventData)
     {
+
         AudioMgr.Instance.PlaySfx("黑胶");
         // 打开留声机 暂停时间流逝
         currentPauseState = !currentPauseState;
@@ -17,6 +18,7 @@ public class PhonographUIScript : MonoBehaviour, IPointerClickHandler
             AudioMgr.Instance.ResumeBgm();
 
         NoteMgr.instance.SetPauseState(currentPauseState);
-
+        // 点击过就标记开始
+        NoteMgr.instance.IsStart = true;
     }
 }
