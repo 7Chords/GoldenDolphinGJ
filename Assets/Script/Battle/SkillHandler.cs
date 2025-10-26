ca4b4e29b311fc54ee24de82515adc2149562be8
四不和声 <1223736337@qq.com>
2025-10-26 20:26:41 +0800

    dev:合击

--- Assets/Script/Battle/SkillHandler.cs
+++ Assets/Script/Battle/SkillHandler.cs
@@ -7,6 +7,8 @@ using UnityEngine;
 /// </summary>
 public static class SkillHandler
 {
+
+    public static float bounceRate = 0.2f; 
     public static void DealTogetherSkill(List<IDamagable> senderList,SkillRefObj skillRefObj)
     {
         if (senderList == null
@@ -20,16 +22,99 @@ public static class SkillHandler
         switch(skillRefObj.skillName)
         {
             case "小号和小提琴的合击":
+                {
+                    int attackerDamage = 0;
+                    int totalDamage = 0;
+                    for(int i =0;i<senderList.Count;i++)
+                    {
+                        if((senderList[i] as InstrumentItem).instrumentInfo.refObj.effectType == EInstrumentEffectType.Attack)
+                        {
+                            attackerDamage = (senderList[i] as InstrumentItem).instrumentInfo.attack;
+                            totalDamage += attackerDamage;
+                        }
+                        else if((senderList[i] as InstrumentItem).instrumentInfo.refObj.effectType == EInstrumentEffectType.Buff)
+                        {
+                            totalDamage += (senderList[i] as InstrumentItem).instrumentInfo.buff;
+                        }
+                    }
+                    totalDamage += (int)(attackerDamage * 0.5f);
+                    BattleMgr.instance.enemyItem.TakeDamage(totalDamage);
+                }
                 break;
             case "小号和手风琴的合击":
+                {
+                    int attackerDamage = 0;
+                    int totalDamage = 0;
+                    for (int i = 0; i < senderList.Count; i++)
+                    {
+                        if ((senderList[i] as InstrumentItem).instrumentInfo.refObj.effectType == EInstrumentEffectType.Attack)
+                        {
+                            attackerDamage = (senderList[i] as InstrumentItem).instrumentInfo.attack;
+                            totalDamage += attackerDamage;
+                        }
+                        else if ((senderList[i] as InstrumentItem).instrumentInfo.refObj.effectType == EInstrumentEffectType.Buff)
+                        {
+                            totalDamage += (senderList[i] as InstrumentItem).instrumentInfo.buff;
+                        }
+                    }
+                    totalDamage += (int)(attackerDamage * 0.5f);
+                    BattleMgr.instance.enemyItem.TakeDamage(totalDamage);
+                }
                 break;
             case "小号和单簧管的合击":
+                {
+                    int attackerDamage = 0;
+                    int totalDamage = 0;
+                    for (int i = 0; i < senderList.Count; i++)
+                    {
+                        if ((senderList[i] as InstrumentItem).instrumentInfo.refObj.effectType == EInstrumentEffectType.Attack)
+                        {
+                            attackerDamage = (senderList[i] as InstrumentItem).instrumentInfo.attack;
+                            totalDamage += attackerDamage;
+                        }
+                        else if ((senderList[i] as InstrumentItem).instrumentInfo.refObj.effectType == EInstrumentEffectType.Buff)
+                        {
+                            totalDamage += (senderList[i] as InstrumentItem).instrumentInfo.buff;
+                        }
+                    }
+                    totalDamage += (int)(attackerDamage * 0.5f);
+                    BattleMgr.instance.enemyItem.TakeDamage(totalDamage);
+                }
                 break;
             case "长笛和小提琴的合击":
+                {
+                    int totalDamage = 0;
+                    for (int i = 0; i < senderList.Count; i++)
+                    {
+                        totalDamage += (senderList[i] as InstrumentItem).instrumentInfo.attack;
+                    }
+                    BattleMgr.instance.enemyItem.TakeDamage(totalDamage);
+                    bounceRate = 0.4f;
+                }
                 break;
             case "长笛和手风琴的合击":
+                {
+                    int totalDamage = 0;
+                    for (int i = 0; i < senderList.Count; i++)
+                    {
+                        totalDamage += (senderList[i] as InstrumentItem).instrumentInfo.attack;
+                    }
+                    BattleMgr.instance.enemyItem.TakeDamage(totalDamage);
+                    bounceRate = 0.4f;
+
+                }
                 break;
             case "长笛和单簧管的合击":
+                {
+                    int totalDamage = 0;
+                    for (int i = 0; i < senderList.Count; i++)
+                    {
+                        totalDamage += (senderList[i] as InstrumentItem).instrumentInfo.attack;
+                    }
+                    BattleMgr.instance.enemyItem.TakeDamage(totalDamage);
+                    bounceRate = 0.4f;
+
+                }
                 break;
             default:
                 break;
@@ -55,8 +140,9 @@ public static class SkillHandler
                     EnemyItem enemyItem = BattleMgr.instance.enemyItem;
                     if (enemyItem != null)
                     {
-                        enemyItem.TakeDamage(damage / 2);
+                        enemyItem.TakeDamage((int)(damage * bounceRate));
                     }
+                    bounceRate = 0.2f;
                 });
                 break;
             default:
