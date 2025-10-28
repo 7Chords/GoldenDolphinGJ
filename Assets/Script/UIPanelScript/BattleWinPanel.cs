3d3df937f491ec34da21babf1917493cf04eedb9
四不和声 <1223736337@qq.com>
2025-10-28 22:50:38 +0800

    dev:素材

--- Assets/Script/UIPanelScript/BattleWinPanel.cs
+++ Assets/Script/UIPanelScript/BattleWinPanel.cs
@@ -14,6 +14,9 @@ public class BattleWinPanel : UIPanelBase
     public float fadeInDuration;
     public float fadeOutDuration;
     public Button btnConfirm;
+    public Image imgBg;
+    public GameObject goNoLastTile;
+    public GameObject goLastTile;
 
     protected override void OnShow()
     {
@@ -21,12 +24,31 @@ public class BattleWinPanel : UIPanelBase
         _tweenContainer = new TweenContainer();
         _tweenContainer.RegDoTween(canvasGroup.DOFade(1, fadeInDuration));
 
-/*        btnConfirm.onClick.AddListener(() =>
+        /*        btnConfirm.onClick.AddListener(() =>
+                {
+                    AudioMgr.Instance.PlayBgm("背景音乐");
+                    SceneLoader.Instance.AddNextScenePanel(EPanelType.LevelSelectPanel);
+                    TransitionMgr.Instance.StarTransition("LevelSelectScene", "FadeInAndOutTransition");
+                });*/
+        BattleLevelRefObj levelRefObj = SCRefDataMgr.Instance.battleLevelRefList.refDataList
+            .Find(x => x.level == GameMgr.Instance.curLevel);
+        if (levelRefObj != null)
         {
-            AudioMgr.Instance.PlayBgm("背景音乐");
-            SceneLoader.Instance.AddNextScenePanel(EPanelType.LevelSelectPanel);
-            TransitionMgr.Instance.StarTransition("LevelSelectScene", "FadeInAndOutTransition");
-        });*/
+            ResultResRefObj resultRefObj = SCRefDataMgr.Instance.resultResRefList.refDataList.Find(x => x.id == levelRefObj.resultSkinId);
+            if (resultRefObj != null)
+                imgBg.sprite = Resources.Load<Sprite>(resultRefObj.winBgPath);
+        }
+        if(GameMgr.Instance.curLevel == 3)//临时的写法
+        {
+            goLastTile.SetActive(true);
+            goNoLastTile.SetActive(false);
+        }
+        else
+        {
+            goLastTile.SetActive(false);
+            goNoLastTile.SetActive(true);
+        }
+
     }
     protected override void OnHide(Action onHideFinished)
     {
