b9b8a94299675e04ec8f3316f089417c516c8146
四不和声 <1223736337@qq.com>
2025-10-30 00:30:04 +0800

    fix：收集界面默认不播放音频

--- Assets/Script/Note/NoteUIScript/PhonographUIScript.cs
+++ Assets/Script/Note/NoteUIScript/PhonographUIScript.cs
@@ -6,19 +6,35 @@ using UnityEngine.UI;
 public class PhonographUIScript : MonoBehaviour, IPointerClickHandler
 {
     bool currentPauseState = true;
+
+    bool hasStartPlay = false;
     public void OnPointerClick(PointerEventData eventData)
     {
 
         AudioMgr.Instance.PlaySfx("黑胶");
         // 打开留声机 暂停时间流逝
         currentPauseState = !currentPauseState;
-        if (currentPauseState)
-            AudioMgr.Instance.PauseBgm();
+
+        if(!hasStartPlay)
+        {
+            hasStartPlay = true;
+            AudioMgr.Instance.PlayBgm("土耳其");
+        }
         else
-            AudioMgr.Instance.ResumeBgm();
+        {
+            if (currentPauseState)
+                AudioMgr.Instance.PauseBgm();
+            else
+                AudioMgr.Instance.ResumeBgm();
+        }
 
         NoteMgr.instance.SetPauseState(currentPauseState);
         // 点击过就标记开始
         NoteMgr.instance.IsStart = true;
+    }
+
+    private void OnEnable()
+    {
+        hasStartPlay = false;
     }
 }
